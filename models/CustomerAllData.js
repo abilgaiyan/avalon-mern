6f66e52bcc927343b01cf2ddb84d90fe3421eb61
mayank patni <mayankpatni@AVALONSOLUTIONS.CO.IN>
2018-09-27 12:30:29 +0530

    Data added in Model

---
+++ models/CustomerAllData.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+//const CustomerSchema = require('./Customer');
+//const AddressSchema = require('./Address');
+
+//define schema for our customer
+
+const customeralldataSchema = new Schema({
+    jewelsoftId: String,
+    customerName: String,
+    websiteUrl: String,
+    contactPersonName: String,
+    address1: String,
+    city: String,
+    state: String,
+    telephone: Number,
+    customerStatusType = String,
+    websiteReleaseMonth = String,
+    websiteReleaseYear = String,
+    responsiveReleaseDate = Date,
+    salesPersonName: String,
+    liquidDesign: String,
+    monthlyPlan: String,
+    hostingAmount: String,
+    holdyear: String,
+    yearlyPlan: String,
+    responsiveUpgrade: String,
+    websiteStatus: String,
+    filterSearchFunctionality: String,
+    adminDeploymentStatus: String,
+    responsiveWebsiteWithSmallSlider: String,
+});
+// create modal based on schema
+mongoose.model('customerdata', customeralldataSchema);
