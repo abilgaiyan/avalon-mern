f8dc49eecbd7f4a9d3e90409e3533c96337ca4c9
abilgaiyan <abilgaiyan@gmail.com>
2018-10-22 18:34:50 +0530

    Login || Logout Redirection

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../actions";
 
@@ -20,11 +20,16 @@ class App extends Component {
   }
 
   render() {
+    //console.log(this.props.auth !== false);
+
     return (
       <BrowserRouter>
         <div id="wrapper" className="dashboard_wraper container-fluid">
+          {this.props.auth !== false ? <Redirect push to="/customers" /> : <Redirect push to="/" />}
           <Route exact={true} path="/" component={Welcome} />
-          
+
+
+
           {/* Main Wraper Start Here */}
           {window.location.pathname === "/" ? null : (
             <div>
@@ -32,19 +37,13 @@ class App extends Component {
                 <LeftSideBar />
                 <div className="main_containt col-sm-10">
                   <div className="container-fluid">
-                    {window.location.pathname === "/" ? null : <Header />}
-                    <Route
-                      exact={true}
-                      path="/customers"
-                      component={Dashboard}
-                    />
-                    <Route
-                      exact={true}
-                      path="/customers/:customerId"
-                      component={CustomerDetails}
-                    />
-                   
+                    {window.location.pathname === "/" || this.props.auth === false ? null : <Header />}
+                    <Route exact={true} path="/customers" component={Dashboard} />
+                    <Route exact={true} path="/customers/:customerId" component={CustomerDetails} />
                     <Route exact={true} path="/emailForm" component={Email} />
+                    {/* <Redirect push to="/customers" /> */}
+
+
                   </div>
                 </div>
               </div>
@@ -61,7 +60,13 @@ class App extends Component {
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+  };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(App);
