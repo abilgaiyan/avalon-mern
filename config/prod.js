84b05095da208b0ea55d9ed35cfcad60d7bb16ea
abilgaiyan <abilgaiyan@gmail.com>
2018-12-01 17:32:55 +0530

    deployment changes package.json

--- config/prod.js
+++ config/prod.js
@@ -1,13 +1,25 @@
 // Keys for production --  commit it
-module.exports ={
-    googleClientId: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongodbURL: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY,
-    stripePubilishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-    sendGridApiKey: process.env.SENDGRID_API_KEY,
-    redirectDomain: process.env.REDIRECT_DOMAIN,
-    videourlpath: process.env.VIDEO_URL_PATH,
-    posterurlpath: process.env.POSTER_URL_PATH
+// module.exports ={
+//     googleClientId: process.env.GOOGLE_CLIENT_ID,
+//     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     mongodbURL: process.env.MONGO_URI,
+//     cookieKey: process.env.COOKIE_KEY,
+//     stripePubilishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+//     stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+//     sendGridApiKey: process.env.SENDGRID_API_KEY,
+//     redirectDomain: process.env.REDIRECT_DOMAIN,
+//     videourlpath: process.env.VIDEO_URL_PATH,
+//     posterurlpath: process.env.POSTER_URL_PATH
+// }
+
+module.exports = {
+    googleClientId:  '208428113244-dp7sh6h5v2j5s7u1ko5vup62iivn9if5.apps.googleusercontent.com',
+    googleClientSecret: 'gtGhZj1UKNEDqGAjPGf-D8E8',
+    mongodbURL: 'mongodb://avalon:avalon123@ds113693.mlab.com:13693/avalonsupport-dev',
+    cookieKey: 'mycookiesecret',
+    stripePublishableKey: 'pk_test_92uJh3eGZTKmbQTpOV2AydNw',
+    stripeSecretKey: 'sk_test_CAbFpdPtBTTfAk94c5FgImRG',
+    sendGridApiKey: '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',
+    redirectDomain: 'http://crm.avalonsolutions.co.in:3000/',
+    videourlpath: '/client/public/videourl/'
 }
